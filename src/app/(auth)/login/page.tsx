323cbfdfe6597bdd6ffa5f7ce5be1017e3a07696
Colby Garland <colbygarland@gmail.com>
2025-03-31 21:50:03 -0600

    update next; move out laravel stuff

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import Button from '@/components/Button'
-import Input from '@/components/Input'
-import InputError from '@/components/InputError'
-import Label from '@/components/Label'
+import Button from '@/components/__DO_NOT_USE/Button'
+import Input from '@/components/__DO_NOT_USE/Input'
+import InputError from '@/components/__DO_NOT_USE/InputError'
+import Label from '@/components/__DO_NOT_USE/Label'
 import Link from 'next/link'
 import { useAuth } from '@/hooks/auth'
 import { useEffect, useState } from 'react'
@@ -11,114 +11,108 @@ import { useRouter } from 'next/navigation'
 import AuthSessionStatus from '@/app/(auth)/AuthSessionStatus'
 
 const Login = () => {
-    const router = useRouter()
+  const router = useRouter()
 
-    const { login } = useAuth({
-        middleware: 'guest',
-        redirectIfAuthenticated: '/dashboard',
-    })
+  const { login } = useAuth({
+    middleware: 'guest',
+    redirectIfAuthenticated: '/dashboard',
+  })
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [shouldRemember, setShouldRemember] = useState(false)
-    const [errors, setErrors] = useState([])
-    const [status, setStatus] = useState(null)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [shouldRemember, setShouldRemember] = useState(false)
+  const [errors, setErrors] = useState([])
+  const [status, setStatus] = useState(null)
 
-    useEffect(() => {
-        // @ts-expect-error from the scaffolding
-        if (router.reset?.length > 0 && errors.length === 0) {
-            // @ts-expect-error from the scaffolding
-            setStatus(atob(router.reset))
-        } else {
-            setStatus(null)
-        }
-    })
-
-    const submitForm = async event => {
-        event.preventDefault()
-
-        login({
-            email,
-            password,
-            remember: shouldRemember,
-            setErrors,
-            setStatus,
-        })
+  useEffect(() => {
+    // @ts-expect-error from the scaffolding
+    if (router.reset?.length > 0 && errors.length === 0) {
+      // @ts-expect-error from the scaffolding
+      setStatus(atob(router.reset))
+    } else {
+      setStatus(null)
     }
+  })
 
-    return (
-        <>
-            <AuthSessionStatus className="mb-4" status={status} />
-            <form onSubmit={submitForm}>
-                {/* Email Address */}
-                <div>
-                    <Label htmlFor="email">Email</Label>
+  const submitForm = async event => {
+    event.preventDefault()
 
-                    <Input
-                        id="email"
-                        type="email"
-                        value={email}
-                        className="block mt-1 w-full"
-                        onChange={event => setEmail(event.target.value)}
-                        required
-                        autoFocus
-                    />
+    login({
+      email,
+      password,
+      remember: shouldRemember,
+      setErrors,
+      setStatus,
+    })
+  }
 
-                    {/* @ts-expect-error from the scaffolding */}
-                    <InputError messages={errors.email} className="mt-2" />
-                </div>
+  return (
+    <>
+      <AuthSessionStatus className="mb-4" status={status} />
+      <form onSubmit={submitForm}>
+        {/* Email Address */}
+        <div>
+          <Label htmlFor="email">Email</Label>
 
-                {/* Password */}
-                <div className="mt-4">
-                    <Label htmlFor="password">Password</Label>
+          <Input
+            id="email"
+            type="email"
+            value={email}
+            className="mt-1 block w-full"
+            onChange={event => setEmail(event.target.value)}
+            required
+            autoFocus
+          />
 
-                    <Input
-                        id="password"
-                        type="password"
-                        value={password}
-                        className="block mt-1 w-full"
-                        onChange={event => setPassword(event.target.value)}
-                        required
-                        autoComplete="current-password"
-                    />
+          {/* @ts-expect-error from the scaffolding */}
+          <InputError messages={errors.email} className="mt-2" />
+        </div>
 
-                    {/* @ts-expect-error from the scaffolding */}
-                    <InputError messages={errors.password} className="mt-2" />
-                </div>
+        {/* Password */}
+        <div className="mt-4">
+          <Label htmlFor="password">Password</Label>
 
-                {/* Remember Me */}
-                <div className="block mt-4">
-                    <label
-                        htmlFor="remember_me"
-                        className="inline-flex items-center">
-                        <input
-                            id="remember_me"
-                            type="checkbox"
-                            name="remember"
-                            className="rounded-sm border-gray-300 text-indigo-600 shadow-xs focus:border-indigo-300 focus:ring-3 focus:ring-indigo-200 focus:ring-opacity-50"
-                            onChange={event =>
-                                setShouldRemember(event.target.checked)
-                            }
-                        />
+          <Input
+            id="password"
+            type="password"
+            value={password}
+            className="mt-1 block w-full"
+            onChange={event => setPassword(event.target.value)}
+            required
+            autoComplete="current-password"
+          />
 
-                        <span className="ml-2 text-sm text-gray-600">
-                            Remember me
-                        </span>
-                    </label>
-                </div>
+          {/* @ts-expect-error from the scaffolding */}
+          <InputError messages={errors.password} className="mt-2" />
+        </div>
 
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href="/forgot-password"
-                        className="underline text-sm text-gray-600 hover:text-gray-900">
-                        Forgot your password?
-                    </Link>
+        {/* Remember Me */}
+        <div className="mt-4 block">
+          <label htmlFor="remember_me" className="inline-flex items-center">
+            <input
+              id="remember_me"
+              type="checkbox"
+              name="remember"
+              className="focus:ring-opacity-50 rounded-sm border-gray-300 text-indigo-600 shadow-xs focus:border-indigo-300 focus:ring-3 focus:ring-indigo-200"
+              onChange={event => setShouldRemember(event.target.checked)}
+            />
 
-                    <Button className="ml-3">Login</Button>
-                </div>
-            </form>
-        </>
-    )
+            <span className="ml-2 text-sm text-gray-600">Remember me</span>
+          </label>
+        </div>
+
+        <div className="mt-4 flex items-center justify-end">
+          <Link
+            href="/forgot-password"
+            className="text-sm text-gray-600 underline hover:text-gray-900">
+            Forgot your password?
+          </Link>
+
+          <Button className="ml-3">Login</Button>
+        </div>
+      </form>
+    </>
+  )
 }
 
 export default Login
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,125 +1,123 @@
 'use client'
 
-import Button from '@/components/Button'
-import Input from '@/components/Input'
-import InputError from '@/components/InputError'
-import Label from '@/components/Label'
+import Button from '@/components/__DO_NOT_USE/Button'
+import Input from '@/components/__DO_NOT_USE/Input'
+import InputError from '@/components/__DO_NOT_USE/InputError'
+import Label from '@/components/__DO_NOT_USE/Label'
 import Link from 'next/link'
 import { useAuth } from '@/hooks/auth'
 import { useState } from 'react'
 
 const Page = () => {
-    const { register } = useAuth({
-        middleware: 'guest',
-        redirectIfAuthenticated: '/dashboard',
+  const { register } = useAuth({
+    middleware: 'guest',
+    redirectIfAuthenticated: '/dashboard',
+  })
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+  const [errors, setErrors] = useState([])
+
+  const submitForm = event => {
+    event.preventDefault()
+
+    register({
+      name,
+      email,
+      password,
+      password_confirmation: passwordConfirmation,
+      setErrors,
     })
+  }
 
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [passwordConfirmation, setPasswordConfirmation] = useState('')
-    const [errors, setErrors] = useState([])
+  return (
+    <form onSubmit={submitForm}>
+      {/* Name */}
+      <div>
+        <Label htmlFor="name">Name</Label>
 
-    const submitForm = event => {
-        event.preventDefault()
+        <Input
+          id="name"
+          type="text"
+          value={name}
+          className="mt-1 block w-full"
+          onChange={event => setName(event.target.value)}
+          required
+          autoFocus
+        />
 
-        register({
-            name,
-            email,
-            password,
-            password_confirmation: passwordConfirmation,
-            setErrors,
-        })
-    }
+        {/* @ts-expect-error from the scaffolding */}
+        <InputError messages={errors.name} className="mt-2" />
+      </div>
 
-    return (
-        <form onSubmit={submitForm}>
-            {/* Name */}
-            <div>
-                <Label htmlFor="name">Name</Label>
+      {/* Email Address */}
+      <div className="mt-4">
+        <Label htmlFor="email">Email</Label>
 
-                <Input
-                    id="name"
-                    type="text"
-                    value={name}
-                    className="block mt-1 w-full"
-                    onChange={event => setName(event.target.value)}
-                    required
-                    autoFocus
-                />
+        <Input
+          id="email"
+          type="email"
+          value={email}
+          className="mt-1 block w-full"
+          onChange={event => setEmail(event.target.value)}
+          required
+        />
 
-                {/* @ts-expect-error from the scaffolding */}
-                <InputError messages={errors.name} className="mt-2" />
-            </div>
+        {/* @ts-expect-error from the scaffolding */}
+        <InputError messages={errors.email} className="mt-2" />
+      </div>
 
-            {/* Email Address */}
-            <div className="mt-4">
-                <Label htmlFor="email">Email</Label>
+      {/* Password */}
+      <div className="mt-4">
+        <Label htmlFor="password">Password</Label>
 
-                <Input
-                    id="email"
-                    type="email"
-                    value={email}
-                    className="block mt-1 w-full"
-                    onChange={event => setEmail(event.target.value)}
-                    required
-                />
+        <Input
+          id="password"
+          type="password"
+          value={password}
+          className="mt-1 block w-full"
+          onChange={event => setPassword(event.target.value)}
+          required
+          autoComplete="new-password"
+        />
 
-                {/* @ts-expect-error from the scaffolding */}
-                <InputError messages={errors.email} className="mt-2" />
-            </div>
+        {/* @ts-expect-error from the scaffolding */}
+        <InputError messages={errors.password} className="mt-2" />
+      </div>
 
-            {/* Password */}
-            <div className="mt-4">
-                <Label htmlFor="password">Password</Label>
+      {/* Confirm Password */}
+      <div className="mt-4">
+        <Label htmlFor="passwordConfirmation">Confirm Password</Label>
 
-                <Input
-                    id="password"
-                    type="password"
-                    value={password}
-                    className="block mt-1 w-full"
-                    onChange={event => setPassword(event.target.value)}
-                    required
-                    autoComplete="new-password"
-                />
+        <Input
+          id="passwordConfirmation"
+          type="password"
+          value={passwordConfirmation}
+          className="mt-1 block w-full"
+          onChange={event => setPasswordConfirmation(event.target.value)}
+          required
+        />
 
-                {/* @ts-expect-error from the scaffolding */}
-                <InputError messages={errors.password} className="mt-2" />
-            </div>
+        <InputError
+          // @ts-expect-error from the scaffolding
+          messages={errors.password_confirmation}
+          className="mt-2"
+        />
+      </div>
 
-            {/* Confirm Password */}
-            <div className="mt-4">
-                <Label htmlFor="passwordConfirmation">Confirm Password</Label>
+      <div className="mt-4 flex items-center justify-end">
+        <Link
+          href="/login"
+          className="text-sm text-gray-600 underline hover:text-gray-900">
+          Already registered?
+        </Link>
 
-                <Input
-                    id="passwordConfirmation"
-                    type="password"
-                    value={passwordConfirmation}
-                    className="block mt-1 w-full"
-                    onChange={event =>
-                        setPasswordConfirmation(event.target.value)
-                    }
-                    required
-                />
-
-                <InputError
-                    // @ts-expect-error from the scaffolding
-                    messages={errors.password_confirmation}
-                    className="mt-2"
-                />
-            </div>
-
-            <div className="flex items-center justify-end mt-4">
-                <Link
-                    href="/login"
-                    className="underline text-sm text-gray-600 hover:text-gray-900">
-                    Already registered?
-                </Link>
-
-                <Button className="ml-4">Register</Button>
-            </div>
-        </form>
-    )
+        <Button className="ml-4">Register</Button>
+      </div>
+    </form>
+  )
 }
 
 export default Page
